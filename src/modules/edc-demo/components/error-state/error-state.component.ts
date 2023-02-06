aefd850f9a10850f1b2dbdf40147b5aa252336a4
AnurosePrakash <anurose.prakash@sovity.de>
2023-02-06 10:30:19 +0100

    refactor: added component for loading and error states of progress loading functionality

---
+++ src/modules/edc-demo/components/error-state/error-state.component.ts
@@ -0,0 +1,16 @@
+import {Component, HostBinding, Input} from '@angular/core';
+
+@Component({
+  selector: 'edc-demo-error-state',
+  templateUrl: './error-state.component.html',
+})
+export class ErrorStateComponent {
+  @HostBinding('class.flex')
+  @HostBinding('class.flex-col')
+  @HostBinding('class.justify-center')
+  @HostBinding('class.h-[700px]')
+  cls = true;
+
+  @Input()
+  data = '';
+}
