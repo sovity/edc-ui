b58298682a32703c5a19748b3cc8b9b51314b65d
Richard Treier <richard.treier@sovity.de>
2023-08-22 11:38:26 +0200

    feat: type-safe fake backend

---
+++ src/app/core/services/api/fake-backend/utils/request-utils.ts
@@ -0,0 +1,13 @@
+export const getUrl = (input: Request | string, baseUrl: string): string => {
+  let url = typeof input === 'string' ? input : input.url;
+  url = url.startsWith(baseUrl) ? url.substr(baseUrl.length) : url;
+  return url;
+};
+
+export const getMethod = (init: RequestInit | undefined): string =>
+  init?.method ?? 'GET';
+
+export const getBody = (input: RequestInit | undefined): null | any => {
+  let body = input?.body?.toString();
+  return body ? JSON.parse(body) : null;
+};
