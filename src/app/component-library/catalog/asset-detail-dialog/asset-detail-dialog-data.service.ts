d1f1ba388e12dfcacf29c3c94824298c8aa81c0e
Richard Treier <richard.treier@sovity.de>
2023-09-25 14:37:50 +0200

    reverted some files

--- src/app/component-library/catalog/asset-detail-dialog/asset-detail-dialog-data.service.ts
+++ src/app/component-library/catalog/asset-detail-dialog/asset-detail-dialog-data.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
-import {UiContractOffer, UiDataOffer} from '@sovity.de/edc-client';
-import {AssetPropertyMapper} from '../../../core/services/asset-property-mapper';
 import {Asset} from '../../../core/services/models/asset';
+import {ContractOffer} from '../../../core/services/models/contract-offer';
 import {BrokerDataOffer} from '../../../routes/broker-ui/catalog-page/catalog-page/mapping/broker-data-offer';
 import {ContractAgreementCardMapped} from '../../../routes/connector-ui/contract-agreement-page/contract-agreement-cards/contract-agreement-card-mapped';
 import {AssetDetailDialogData} from './asset-detail-dialog-data';
@@ -10,7 +9,6 @@ import {AssetPropertyGridGroupBuilder} from './asset-property-grid-group-builder
 @Injectable()
 export class AssetDetailDialogDataService {
   constructor(
-    private assetPropertyMapper: AssetPropertyMapper,
     private assetPropertyGridGroupBuilder: AssetPropertyGridGroupBuilder,
   ) {}
 
@@ -28,7 +26,8 @@ export class AssetDetailDialogDataService {
     };
   }
 
-  contractOfferDetails(contractOffer: UiContractOffer): AssetDetailDialogData {
+  contractOfferDetails(contractOffer: ContractOffer): AssetDetailDialogData {
+    let asset = contractOffer.asset;
     let contractPolicy = contractOffer.policy;
 
     const propertyGridGroups = [
--- src/app/core/services/models/contract-offer-dto.ts
+++ src/app/core/services/models/contract-offer-dto.ts
@@ -1,11 +1,12 @@
-import {UiAsset, UiPolicy} from '@sovity.de/edc-client';
+import {UiAsset} from '@sovity.de/edc-client';
+import {Policy} from '../api/legacy-managent-api-client';
 
 /**
  * Contract Offer (API Model)
  */
 export interface ContractOfferDto {
   id: string;
-  policy: UiPolicy;
+  policy: Policy;
   provider: string;
   consumer: string;
   asset: UiAsset;
