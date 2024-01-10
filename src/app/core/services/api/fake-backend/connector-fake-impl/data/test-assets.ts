f8f0cfdf17fd6696f8ba9a4da3c412b4da0ad24c
kulgg <75735874+kulgg@users.noreply.github.com>
2024-01-10 14:50:17 +0100

    Fix table styling

--- src/app/core/services/api/fake-backend/connector-fake-impl/data/test-assets.ts
+++ src/app/core/services/api/fake-backend/connector-fake-impl/data/test-assets.ts
@@ -53,10 +53,12 @@ export namespace TestAssets {
   [Sovity Website](https://sovity.de/)
   
   **Table**
-| Syntax      | Description |
-| ----------- | ----------- |
-| Header      | Title       |
-| Paragraph   | Text        |
+| Item              | In Stock | Price | Description |
+| :---------------- | :------: | ----: | :---------- |
+| Python Hat        |   True   | 24.99 | This is a long description to test the scrolling behavior of the table. |
+| SQL Hat           |   True   | 24.99 | This is a long description to test the scrolling behavior of the table. |
+| Codecademy Tee    |  False   | 20.99 | This is a long description to test the scrolling behavior of the table. |
+| Codecademy Hoodie |  False   | 43.99 | This is a long description to test the scrolling behavior of the table. |
 `;
 
   export const boring: UiAsset = {
