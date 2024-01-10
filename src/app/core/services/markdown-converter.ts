d8a4d7b6ad6092d4b287932ab40c24fccbd6a609
kulgg <75735874+kulgg@users.noreply.github.com>
2024-01-10 15:10:34 +0100

    .

--- src/app/core/services/markdown-converter.ts
+++ src/app/core/services/markdown-converter.ts
@@ -1,10 +1,9 @@
 import {Injectable} from '@angular/core';
-import {sanitize} from 'isomorphic-dompurify';
 import {marked} from 'marked';
 
 @Injectable({providedIn: 'root'})
 export class MarkdownConverter {
   toHtml(markdown: string) {
-    return sanitize(marked.parse(markdown).toString());
+    return marked.parse(markdown).toString();
   }
 }
