acce6c17f8c9e5d63188c8848fda910009e01761
kulgg <75735874+kulgg@users.noreply.github.com>
2024-01-11 09:34:39 +0100

    Improve table styling

--- src/app/core/services/api/fake-backend/connector-fake-impl/data/test-assets.ts
+++ src/app/core/services/api/fake-backend/connector-fake-impl/data/test-assets.ts
@@ -48,10 +48,10 @@ export namespace TestAssets {
   **Table**
 | Item              | In Stock | Price | Description |
 | :---------------- | :------: | ----: | :---------- |
-| Python Hat        |   True   | 24.99 | This is a long description to test the scrolling behavior of the table. |
-| SQL Hat           |   True   | 24.99 | This is a long description to test the scrolling behavior of the table. |
-| Codecademy Tee    |  False   | 20.99 | This is a long description to test the scrolling behavior of the table. |
-| Codecademy Hoodie |  False   | 43.99 | This is a long description to test the scrolling behavior of the table. |
+| Python Hat        |   True   | 24.99 | This is a long description to test the scrolling behavior of the table. This is a long description to test the scrolling behavior of the table. |
+| SQL Hat           |   True   | 24.99 | This is a long description to test the scrolling behavior of the table. This is a long description to test the scrolling behavior of the table. |
+| Codecademy Tee    |  False   | 20.99 | This is a long description to test the scrolling behavior of the table. This is a long description to test the scrolling behavior of the table. |
+| Codecademy Hoodie |  False   | 43.99 | This is a long description to test the scrolling behavior of the table. This is a long description to test the scrolling behavior of the table. |
 `;
 
   export const boring: UiAsset = {
