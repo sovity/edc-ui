bd56e2ef9a7a4a615fdfe07c9c303390beb3dd0e
illfixit <66363651+illfixit@users.noreply.github.com>
2024-09-02 15:57:48 +0200

    chore: refactor parse using date-fns

--- src/app/core/adapters/custom-date-adapter.ts
+++ src/app/core/adapters/custom-date-adapter.ts
@@ -1,17 +1,14 @@
 import {NativeDateAdapter} from '@angular/material/core';
+import {isValid, parse as parseDate} from 'date-fns';
 import {format} from 'date-fns-tz';
 
 export class CustomDateAdapter extends NativeDateAdapter {
   parse(value: any): Date | null {
     if (typeof value === 'string' && value.indexOf('/') > -1) {
-      const str = value.split('/');
-
-      const year = Number(str[2]);
-      const month = Number(str[1]) - 1;
-      const date = Number(str[0]);
-
-      return new Date(year, month, date);
+      const parsedDate = parseDate(value, 'dd/MM/yyyy', new Date());
+      return isValid(parsedDate) ? parsedDate : null;
     }
+
     const timestamp = typeof value === 'number' ? value : Date.parse(value);
     return isNaN(timestamp) ? null : new Date(timestamp);
   }
